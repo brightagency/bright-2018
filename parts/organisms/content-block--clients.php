3549de62ac4ce22017057ad06dab2fd9668840cf
TreeTrum <sam@sjd.co>
2018-11-10 22:49:24 +1100

    File structuring

---
+++ parts/organisms/content-block--clients.php
@@ -0,0 +1,60 @@
+<section class="content-block content-block--invert content-block--bg-red">
+    <div class="container">
+        <div class="row">
+            <div class="large-6 columns">
+                <h2 class="section-title">Proud to have worked with.</h2>
+                <p class="lead">We love partnering with organisations that are passionate about their purpose and take a forward-thinking approach to the impact they can have.</p>
+                <p class="content-block__cta-container"><a href="#" class="button button--hover-red">Our Services</a></p>
+            </div>
+            <div class="large-6 columns">
+                <div class="client-grid">
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/nsw-government.svg" alt="NSW Government">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/pam.svg" alt="Parramatta Mission">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/phn.svg" alt="PHN South Western Sydney">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/achieve.svg" alt="Achieve Australia">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/tad.svg" alt="TAD - Clever Ideas Changing Lives">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/ahr.svg" alt="Australian Hearing">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/wwe.svg" alt="WentWest">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/wsu.svg" alt="Western Sydney University">
+                        </div>
+                    </div>
+                    <div class="client-grid__col">
+                        <div class="client-grid__logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/worked-with/odm.svg" alt="One Door Mental Health">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ parts/organisms/content-block--who.php
@@ -0,0 +1,11 @@
+<section class="content-block content-block--invert" style="background-image: url(<?php echo get_template_directory_uri() ?>/dist/images/wework-bg.jpg)">
+    <div class="container">
+        <div class="row">
+            <div class="medium-8 columns">
+                <div class="content-block__logo"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/logos/bright-logo.svg" alt="Bright Logo"></div>
+                <p class="subtitle"><strong>Who?</strong> We’re a team of problem solvers and creative thinkers with a common passion for dedicating our focus and skills to projects that have a <strong>positive impact</strong> on the lives of others.</p>
+                <p class="content-block__cta-container"><a href="#" class="button">Get in Touch</a></p>
+            </div>
+        </div>
+    </div>
+</section>
