911b60a621d47412004276e866ba2da6b8691a95
TreeTrum <sam@sjd.co>
2018-11-19 20:10:00 +1100

    Add favicon and meta

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.png">
+    <meta name="description" content="We build brands, design websites and produce visual media for progressive companies, government agencies, NGOs and nonprofits">
     <?php wp_head(); ?>
 </head>
 
